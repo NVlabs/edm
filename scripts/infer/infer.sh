f3d4434654364f9a09c9a078125d084ae4229acb
KevinRojas1499 <kevinrojas99@gmail.com>
2023-04-14 12:41:12 +0000

    Training files

--- scripts/infer/infer.sh
+++ scripts/infer/infer.sh
@@ -1,3 +1,3 @@
-torchrun --standalone --nnodes=1 --nproc_per_node=8 generate.py --outdir=ffhq_large_def_out --seeds=0-63 --batch=64 \
-    --network ffhq-training-runs/00057-ffhq-128x128_ffhq-96x96_ffhq-64x64_ffhq-48x48_ffhq-32x32-uncond-ddpmpp-edm-gpus8-batch256-fp32/network-snapshot-023206.pkl \
-    --img_resolution 34
+torchrun --standalone --nnodes=1 --nproc_per_node=8 generate.py --outdir=cifar_out/64 --seeds=0-10 --batch=64 \
+    --network cifar-training-runs/00000-cifar10-32x32-uncond-ddpmpp-edm-gpus8-batch128-fp16/network-snapshot-006256.pkl \
+    --img_resolution 64
